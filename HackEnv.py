9d08677e30d74c30c8be26b5f73ecdf8cdaa0c26
Kali Elaina <kali@new-host-2.home>
2020-09-13 08:41:46 -0400

    Final commit for review

--- HackEnv.py
+++ HackEnv.py
@@ -1,19 +1,48 @@
 import gym, os, subprocess
 from gym import spaces
+import requests
+from bs4 import BeautifulSoup
 
 class HackEnv(gym.Env):
+    # Enum relating commands to numbers for discrete action space
+    # I know it's not an enum but it should be lol
+    commands = {
+        0: "ls",
+        1: "cd",
+        2: "cat",
+        3: "echo",
+        4: "grep"
+    }
     """Custom filesystem environment that follows gym interface"""
+
+    """pull text from an HTML file (for finding relevant words that help with finding flags"""
+    def news(self):
+        # the target we want to open
+        url = 'https://pwnable.tw/challenge/#2'
+
+        # open with GET method
+        resp = requests.get(url)
+
+        # http_respone 200 means OK status
+        if resp.status_code == 200:
+            print("Successfully opened the web page")
+            print("Flags-\n")
+
+            soup = BeautifulSoup(resp.text, 'html.parser')
+            relwords = soup.find("ul", {"class": commands})
+
+            # now we want to print only the text part of the anchor.
+            # find all the elements of a, i.e anchor
+            for i in l.findAll("a"):
+                print(i.text)
+        else:
+            print("Error")
+
+    news()
 
     metadata = {render.modes: ['human']}
 
-    # Enum relating commands to numbers for discrete action space
-    commands = {
-        0 : "ls",
-        1 : "cd",
-        2 : "cat",
-        3 : "echo",
-        4 : "grep"
-    }
+
 
     # Initialize registers
     location, directory, filename, string = "","","",""
